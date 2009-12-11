2e9388aa3e76c46392801db68b855c2150320fc1
Jason Lengstorf <answers@ennuidesign.com>
2009-12-11 14:41:05 -0700

    Added comments, set up conditional statements to only load Google Analytics if a user is set.

--- public/index.php
+++ public/index.php
@@ -1,13 +1,13 @@
 <?php
 	/*
-	 * Starts output buffering
+	 * Starts output buffering and enables GZIP compression
 	 */
 	ob_start("ob_gzhandler");
 
 	/*
 	 * Initializes the core functionality of the CMS
 	 */
-	require_once '../ennui-cms/core/shared.inc.php';
+	require_once '../ennui-cms/core/core.inc.php';
 
 	/*
 	 * Classes used in the sidebar or in widgets need to be included explicitly
@@ -35,6 +35,10 @@
 		href="/assets/css/<?php echo $obj->url0 ?>.css" />
 <?php
 	endif;
+
+	/*
+	 * If the user is logged in, load stylesheets for the admin controls
+	 */
 	if($obj->url0=="admin"
 		|| isset($_SESSION['loggedIn'])
 		&& $_SESSION['loggedIn']==1):
@@ -66,11 +70,11 @@
 		<div id="header">
 			<h1 id="header_title"> <a href="/"><?php echo SITE_NAME; ?></a> </h1>
 <?php echo Utilities::buildMenu($url_array, $menuPages) ?>
-		</div><!-- end header -->
+		</div><!-- end #header -->
 		<div id="content">
 			<div class="entrydisplay" id="<?php echo $obj->url0 ?>">
 <?php echo $obj->displayPublic() ?>
-			</div><!-- end entrydisplay -->
+			</div><!-- end .entrydisplay -->
 			<div id="sidebar">
 				<div id="<?php echo $minibio->url0 ?>"><?php echo $minibio->displayPublic(); ?>
 
@@ -80,29 +84,40 @@
 <?php echo blog::displayRecentPosts() ?>
 					<a href="/blog/category/recent/3/">See More Entries</a>
 				</div><!-- end .entries-list -->
-			</div><!-- end sidebar -->
+			</div><!-- end #sidebar -->
 			<div id="footer">
 				<p class="credits">
 					All content &copy; <?php echo Utilities::copyrightYear(SITE_CREATED_YEAR), ' ', SITE_NAME; ?> |
 					<a href="<?php echo SITE_URL ?>/contact">Contact Us</a> |
 					<a href="http://ennuidesign.com" rel="external">Website by Ennui Design</a>
 				</p>
-			</div><!-- end footer -->
-		</div><!-- end content -->
-	</div><!-- end master -->
+			</div><!-- end #footer -->
+		</div><!-- end #content -->
+	</div><!-- end #master -->
 
 	<script type="text/javascript" src="http://www.google.com/jsapi"></script>
 	<script type="text/javascript">
 		google.load("jquery", "1");
 		google.load("jqueryui", "1");
 	</script>
-<?php if($obj->url0=="admin" || isset($_SESSION['loggedIn']) && $_SESSION['loggedIn']==1): ?>
+<?php
+	/*
+	 * If the user is logged in, load JavaScript for the admin controls
+	 */
+	if($obj->url0=="admin" || isset($_SESSION['loggedIn']) && $_SESSION['loggedIn']==1):
+?>
 	<script type="text/javascript" src="/assets/js/tiny_mce/jquery.tinymce.js"></script>
 	<script type="text/javascript" src="/assets/js/ennui.admin.js"></script>
 	<script type="text/javascript" src="/assets/js/jquery.uploadify.js"></script>
 <?php endif ?>
 	<script type="text/javascript" src="/assets/js/ennui.init.js"></script>
 
+<?php
+	/*
+	 * If a Google Analytics user is set, include the Google Analytics code
+	 */
+	if( GOOGLE_ANALYTICS_USER != "" ):
+?>
 	<!--// Google Analytics //-->
 	<script type="text/javascript" src="http://www.google-analytics.com/ga.js"></script>
 	<script type="text/javascript">
@@ -110,6 +125,7 @@
 		pageTracker._trackPageview();
 	</script>
 	<!--// End Google Analytics //-->
+<?php endif; ?>
 
 </body>
 
