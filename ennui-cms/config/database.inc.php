f829af9f6f4e70cba68be3017b628a5fc5fbf116
Jason Lengstorf <answers@ennuidesign.com>
2009-12-11 14:45:25 -0700

    Added constants for default character sets and collation in DB tables

--- ennui-cms/config/database.inc.php
+++ ennui-cms/config/database.inc.php
@@ -6,12 +6,18 @@
 $_CONSTANTS['DB_HOST'] = 'localhost';
 $_CONSTANTS['DB_USER'] = 'root';
 $_CONSTANTS['DB_PASS'] = '';
-$_CONSTANTS['DB_NAME'] = 'test';
+$_CONSTANTS['DB_NAME'] = 'ennui-cms_db';
 
 /*
  * Assign a custom prefix for all database tables to avoid conflicts
  */
 $_CONSTANTS['DB_PREFIX'] = 'ennui-cms_';
+
+/*
+ * Assign a default character set and collation for the database
+ */
+$_CONSTANTS['DEFAULT_CHARACTER_SET'] = "utf8";
+$_CONSTANTS['DEFAULT_COLLATION'] = "utf8_unicode_ci";
 
 /*
  * If set to TRUE, ADMINUTILITIES::buildDB() is run, which creates the 
