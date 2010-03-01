f39c1594d48100fb3be403632771cde1bc7b0d34
Jason Lengstorf <answers@ennuidesign.com>
2010-03-01 00:33:35 -0700

    Modified the default administrator username and password.

--- ennui-cms/config/admin.inc.php
+++ ennui-cms/config/admin.inc.php
@@ -1,17 +1,22 @@
 <?php
 
 /*
- *  The default administrator is created using this information. No 
- *  confirmation email is sent, and no activation is required; the 
+ * The default administrator is created using this information. No 
+ * confirmation email is sent, and no activation is required; the username and
+ * password can be used immediately to log into the site.
+ *
+ * NOTE: By default, the default administrator login name is "admin"
  */
-$_CONSTANTS['DEV_NAME'] = 'John Doe';
-$_CONSTANTS['DEV_EMAIL'] = 'john.doe@example.com';
+$_CONSTANTS['DEV_NAME'] = 'admin';
+$_CONSTANTS['DEV_EMAIL'] = 'admin@example.com';
 
 /*
  * The SHA1 hash of the default administrator's password. This can be generated 
  * here: http://www.daveproxy.co.uk/tools/sha1_hash_generator.php
+ *
+ * NOTE: The default password is "admin"
  */
-$_CONSTANTS['DEV_PASS'] = 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3';
+$_CONSTANTS['DEV_PASS'] = 'd033e22ae348aeb5660fc2140aec35850c4da997';
 
 /*
  * If set to TRUE, FirePHP logging is enabled and error reporting is set to 
