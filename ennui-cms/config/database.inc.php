3401e5a8817089d39b5312c2d994338adca9bec7
Drew <drew.douglass@ennuidesign.com>
2010-04-26 19:06:30 -0500

    config fix.

--- ennui-cms/config/database.inc.php
+++ ennui-cms/config/database.inc.php
@@ -3,10 +3,10 @@
 /*
  * Database credentials
  */
-$_CONSTANTS['DB_HOST'] = 'DB1985000.db.5362664.hostedresource.com';
-$_CONSTANTS['DB_USER'] = 'DB1985000';
-$_CONSTANTS['DB_PASS'] = 'EDt3stDBASE';
-$_CONSTANTS['DB_NAME'] = 'DB1985000';
+$_CONSTANTS['DB_HOST'] = '';
+$_CONSTANTS['DB_USER'] = '';
+$_CONSTANTS['DB_PASS'] = '';
+$_CONSTANTS['DB_NAME'] = '';
 
 /*
  * Assign a custom prefix for all database tables to avoid conflicts
