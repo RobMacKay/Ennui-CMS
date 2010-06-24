28937842278b6a679d6e0327cbf43ccbfae0d2e6
Drew <drew.douglass@ennuidesign.com>
2010-06-24 00:56:59 -0500

    Added more custom methods to the validate class

--- ennui-cms/core/class.validate.inc.php
+++ ennui-cms/core/class.validate.inc.php
@@ -13,32 +13,56 @@
  */
  class Validate extends Utilities
  {
-	public function __construct()
+ 	public function __construct()
  	{
-		parent::__construct();
- 		}
- 		//------------------------------------------------------------------------------
-		// PUBLIC METHODS
-		//------------------------------------------------------------------------------
-		
-		/**
-		  * Validates an email string, does NOT check mx records.
-		    *
-		      * @param str $email - The email adrress to validate.
-		        * @return bool
-			  *
-			    */
-			    public static function isValidEmail($email)
+ 		parent::__construct();
+ 	}
+ 	//------------------------------------------------------------------------------
+	// PUBLIC METHODS
+	//------------------------------------------------------------------------------
+	
+	/**
+	  * Validates an email string, does NOT check mx records.
+	  *
+	  * @param str $email - The email adrress to validate.
+	  * @return bool
+	  *
+	  */
+	public static function isValidEmail($email)
     {
         // Define a regex pattern to validate the email address
         $pattern = "/^[\w-]+(\.[\w-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i";
         return preg_match($pattern, $email) === 1 ? TRUE : FALSE;
     }
-    //------------------------------------------------------------------------------
-    // PROTECTED METHODS
-    //------------------------------------------------------------------------------
     
-    //------------------------------------------------------------------------------
-    // PRIVATE METHODS
-    //------------------------------------------------------------------------------
+    /**
+      * Check if integer is even, also see isOdd()
+      * Uses bit shifting instead of modulus for speed (and funsies).
+      *
+      * @param int integer to check 
+      * @return bool 
+      */
+      public static function isEven($int)
+      {
+      	return !((int)$int&1) ? TRUE : FALSE;
+      }
+      
+    /**
+      * Check if integer is even, also see isEven()
+      * Uses bit shifting instead of modulus for speed (and funsies).
+      *
+      * @param int integer to check 
+      * @return bool 
+      */
+      public static function isOdd($int)
+      {
+      	return ((int)$int&1) ? TRUE : FALSE;
+      }
+	//------------------------------------------------------------------------------
+	// PROTECTED METHODS
+	//------------------------------------------------------------------------------
+	
+	//------------------------------------------------------------------------------
+	// PRIVATE METHODS
+	//------------------------------------------------------------------------------
  }
